c51e26308bd4f0a317412c4edd954be30691f943
Harold Brenes Hidalgo <hbrenes@outlook.com>
2021-04-29 00:27:03 -0600

    se agrega el sqlserver

--- newLoad.bat
+++ newLoad.bat
@@ -1,2 +1,2 @@
 rem  el ip debe ser el del host de kubernetes
-kubectl run -i --tty load-generator --rm --image=busybox --restart=Never -- /bin/sh -c "while sleep 0.01; do wget -q -O- http://192.168.0.113:30000/cpuinfo; done"
+kubectl run -i --tty load-generator --rm --image=busybox --restart=Never -- /bin/sh -c "while sleep 0.01; do wget -q -O- http://192.168.0.29:30000/cpuinfo; done"
